511e4157164aefe42f51d6dd62eb7db5e2c8270d
Yusuf A <commyusufa@gmail.com>
2025-04-02 01:39:40 -0400

    connected find-meals to meals list

--- src/princetoneats/app.py
+++ src/princetoneats/app.py
@@ -1,9 +1,11 @@
 # Created by Yusuf, Adham, Ndongo, Achilles, Akuei
 
-from flask import Flask, render_template
+# from flask import Flask, render_template
+import flask
+from flask import render_template
 from scrapedining import get_meal_names
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
 @app.route("/")
@@ -13,13 +15,37 @@ def home():
 
 @app.route("/find_meals")
 def find_meals():
-    return render_template("find_meals.html")
+    diningHalls = flask.request.args.getlist("DHfilter")
+    mealTimes = flask.request.args.getlist("MTfilter")
+    print(diningHalls)
+    print(mealTimes)
+
+    linkStringDH = ""
+    linkStringMT = ""
+    print(mealTimes)
+
+    for i in range(len(diningHalls)):
+        linkStringDH += "DHfilter=" + diningHalls[i]
+        linkStringDH += "&"
+
+    for i in range(len(mealTimes)):
+        linkStringMT += "MTfilter=" + mealTimes[i]
+        if i != len(mealTimes) - 1:
+            linkStringMT += "&"
+
+    print(linkStringDH + linkStringMT)
+    return render_template("find_meals.html", MT=linkStringMT, DH=linkStringDH)
 
 
-@app.route("/meals_list")
+@app.route("/meals_list", methods=["GET"])
 def meals_list():
+    diningHall = flask.request.args.getlist("DHfilter")
+    mealTimes = flask.request.args.getlist("MTfilter")
+
+    print(diningHall)
+
     return render_template(
-        "meals_list.html", meals=get_meal_names(["r", "f"], None, "Breakfast")
+        "meals_list.html", meals=get_meal_names(diningHall, None, mealTimes[0])
     )
 
 

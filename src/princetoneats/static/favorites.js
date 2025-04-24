9c1a508d4e6175fe589e657327cccd2617300e14
Adham Ibrahim <adham.ibrahim@princeton.edu>
2025-04-23 22:37:44 -0400

    properly toggle buttons

--- src/princetoneats/static/favorites.js
+++ src/princetoneats/static/favorites.js
@@ -13,3 +13,17 @@ function toggleIsFav(name) {
 function handleError() {
    alert('There\'s been an error.');
 }
+
+const buttons = document.querySelectorAll('.favorites');
+
+// Add event listener to each button
+buttons.forEach(button => {
+   button.addEventListener('click', function() {
+      // Check current text and toggle accordingly
+      if (this.textContent.includes("Add")) {
+         this.textContent = "Remove Favorite";
+      } else if (this.textContent.includes("Remove")) {
+         this.textContent = "Add Favorite";
+      }
+   });
+});
